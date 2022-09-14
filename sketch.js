c891ae9410fdd385d1b849e494f0a95085a9a43d
VladZamStep <vladstep98@yandex.ru>
2022-09-14 12:14:54 +0300

    commit

--- sketch.js
+++ sketch.js
@@ -1,6 +1,22 @@
+let x = 0;
+let y = 0;
+let space = 40;
+
 function setup() {
     createCanvas(500, 500);
     frameRate(60);
-    background(0);
+}
+
+function draw() {
+    stroke(30, 144, 255);
+    if (random(1) < 0.5) {
+        line(x, y, x + space, y + space);
+    }
+    else line(x, y + space, x + space, y);
+    x = x + space;
+    if (x > width) {
+        x = 0;
+        y = y + space;
+    }
 }
 
